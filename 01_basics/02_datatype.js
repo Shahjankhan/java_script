241ce2da1ef8d0f84c18b7b4437d5ee1c6c16215
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2023-11-05 14:35:00 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+// alert(3+3)   //we are using nodejs ,not browser
+
+// console.log(3+3);
+
+
+let name="shahjan"
+let age=18
+let isLoggedIn = false
+
+
+// number ==> 2 to power 53
+//bigint
+// string  ==> " "
+// Boolean  ==> true/false
+// null ==> standalone value
+// undefined
+// Symbol=>uniqe
+
+
+//  --------------  object---------------
+console.log(typeof "shahjan");
+console.log(typeof null);
+console.log(typeof undefined);
