7fbfc28bdaae4e73d615119bf920e84599e90f9a
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2023-11-05 15:11:31 +0000

    conversion of  datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=122142
+let accountEmail="shahjan089gmail.com"
+var accountPassowor="12345"
+accountCity="Meerut"
+
+//  accountId=2 // not allowed
+
+
+console.log(accountId);
+console.log(accountEmail);
+
+/*
+   prefer not to use var 
+   because  of issuse in bolck scope and function scope
+*/
+
+console.table([accountEmail,accountId])
