f0b0fd39cb450829f4bea760f89193a380efb138
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2024-03-24 17:00:43 +0000

    conversion of datatype

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,19 +1,28 @@
-// let score ='33'
+let score ='33'
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
 // let score ="33abc"            //NaN
 // let score =null               //  After converting number the the value is zero
-let score =undefined             // NaN
+// let score =undefined             // NaN
 
 let valueNumber = Number(score)      //  score converting into the number
 
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
 
 
 // "33"   =>33
 //"33abc"  =>NaN
 // true =>1; false => 0
+
+let isLogged ='shaahjna khan'
+let booleanIsLogged=Boolean(isLogged)
+// console.log(booleanIsLogged);
+
+
+let sumNumber=33
+let stringNumber=String(sumNumber)
+console.log( typeof(stringNumber));
