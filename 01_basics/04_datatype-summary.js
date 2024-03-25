21288a8d348e4065e75af2d4d30ea8b5d416fc1b
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2024-03-25 17:12:18 +0000

    datype

---
+++ 01_basics/04_datatype-summary.js
@@ -0,0 +1,27 @@
+// primitive datatype : string, boolean, undefine, bigint ,number,null,symble
+// Nonprimitive (Refrance): array ,object, function
+
+const score=100
+const scoreValue=101.0
+
+const isLoggedIn=true
+const outerTemp=null
+
+let userEmail= 'Shahjankhan089gmailcom';
+
+const id= Symbol('123')
+const anotherId=Symbol('123')
+console.log(id === anotherId);
+
+const heros=['Yaya rana ','saktiman'];
+
+let myObj={
+    name:'Shahjan khan',
+    roll:101,
+    age:24
+}
+
+let  myFunction =function(){
+    console.log('Hello words');
+
+}
