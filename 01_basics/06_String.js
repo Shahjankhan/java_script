a9c12c1e8ebd69d75f794823b3ead21309c885fe
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2024-03-28 17:55:28 +0000

    Added file String

---
+++ 01_basics/06_String.js
@@ -0,0 +1,28 @@
+// console.log('my name is  Shahjan khan');
+
+const name='Shahjan khan'
+const repoCount=10
+// console.log(name + repoCount +  'Values');
+
+// console.log(` Hello My name is ${name} and  repo count ${repoCount}`);
+
+const gameName= new String('Shahjan khan')
+// console.log(` Hello My name is ${gameName} and  repo count ${repoCount}`);
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf('n'));
+
+const newString=gameName.substring(0,2)
+// console.log(newString);
+
+const anotherString=gameName.slice(-12,10)
+// console.log(anotherString);
+
+const newStringOne='   Shahjna.khan   '
+console.log(newStringOne.trim().length);
+
+const url='https://Shahjankhan20%rana.com'
+console.log(url.replace('20%' ,'-'));
