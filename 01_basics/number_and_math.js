b8642644f7c0e396369a5de11eebfadd270f1362
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2024-03-30 15:02:13 +0000

    added new file

---
+++ 01_basics/number_and_math.js
@@ -0,0 +1,21 @@
+const score = 400
+// console.log(score);
+const balance= new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+
+const otherNumber=123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString());
+
+// +++++++++++++++++++++MATH++++++++++++++++++++++
+
+// console.log(Math);
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,2,3,7,8));
+console.log(Math.max(12,11,4,5,78));
