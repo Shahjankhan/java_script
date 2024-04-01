df7bcd5ed2c0c7c1690d1ec1911f8d7897f81feb
shahjan khan <109592380+Shahjankhan@users.noreply.github.com>
2024-04-01 17:26:00 +0000

    Added new file with time aur date

---
+++ 01_basics/date_time.js
@@ -0,0 +1,22 @@
+let myDate= new Date()
+// console.log(myDate);
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate= new Date(25,0,12)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate= new Date('01-12-2025')
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStemp= Date.now()
+// console.log(myCreatedDate.getTime());
+// console.log(myTimeStemp);
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
